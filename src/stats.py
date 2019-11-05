4b04ca5d81559c40471336e13c9ad5336f6fdfd4
busko@stsci.edu <New1trilha>
2019-11-05 16:58:33 -0300

    more stats

--- src/stats.py
+++ src/stats.py
@@ -1,32 +1,58 @@
-import glob, os
+import glob
 
 import rawpy
 import numpy as np
 from numpy import unravel_index
 
 
-# path = '../../astrophotography_data/exposure_tests/tests_1/I6400_10.ARW'
-# path = '../../astrophotography_data/exposure_tests/tests_1/I1600_10.ARW'
-# path = '../../astrophotography_data/MilkyWayPrettyBoy/12800/light/DSC03770.ARW'
-# path = '../../astrophotography_data/dynamic_range/25s_ISO1600_electronic/DSC03434.ARW'
-# path = '../../astrophotography_data/MilkyWayPrettyBoy/800/light/DSC03532.ARW'
+path = '../../astrophotography_data/MilkyWayPrettyBoy/12800/dark/'
 
-# path = '../../astrophotography_data/MilkyWayPrettyBoy/6400/bias/DSC03892.ARW'
-path = '../../astrophotography_data/MilkyWayPrettyBoy/6400/dark/DSC03862.ARW'
+list_p = glob.glob(path + '/*.ARW')
 
-raw = rawpy.imread(path)
-print(raw)
-array = raw.raw_image
+for fname in list_p:
 
-print(array)
+    raw = rawpy.imread(fname)
+    array = raw.raw_image
+    raw_min = np.min(array)
+    raw_max = np.max(array)
+    raw_mean = np.mean(array)
+    raw_std = np.std(array)
 
-raw_min = np.min(array)
-raw_max = np.max(array)
+    pos_min = unravel_index(array.argmin(), array.shape)
+    pos_max = unravel_index(array.argmax(), array.shape)
 
-pos_min = unravel_index(array.argmin(), array.shape)
-pos_max = unravel_index(array.argmax(), array.shape)
+    print('{:s}  {:s} {:s}  {:5d} {:5d}  {:7.2f} {:6.2f}'.format(str(raw.black_level_per_channel),
+        str(pos_min), str(pos_max),
+        raw_min, raw_max, raw_mean, raw_std))
 
-print(raw_min, raw_max, pos_min, pos_max)
+# raw.black_level_per_channel is the bias level. In
+# short exposures dark is not statistically significant,
+# as expected. Lets look for systematic patterns.
+
+summed_array = None
+
+for fname in list_p:
+
+    raw1 = rawpy.imread(fname)
+    array = raw.raw_image
+
+    if summed_array is None:
+        summed_array = np.zeros(shape=array.shape, dtype=float)
+
+    summed_array += array
+
+summed_array /= len(list_p)
+
+print('SUM:')
+raw_min = np.min(summed_array)
+raw_max = np.max(summed_array)
+raw_mean = np.mean(summed_array)
+raw_std = np.std(summed_array)
+
+pos_min = unravel_index(summed_array.argmin(), summed_array.shape)
+pos_max = unravel_index(summed_array.argmax(), summed_array.shape)
+
+print('{:f} {:f}  {:7.2f} {:6.2f}'.format(raw_min, raw_max, raw_mean, raw_std))
 
 
 
