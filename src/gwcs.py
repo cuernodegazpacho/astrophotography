99b98aca21067be9c239918779fd8c4bff8f2058
busko@stsci.edu <New1trilha>
2019-12-30 14:36:18 -0500

    Code to inspect wcs files

--- src/gwcs.py
+++ src/gwcs.py
@@ -5,7 +5,17 @@ from astropy.io import fits
 path = '../../astrophotography_data/MilkyWayPrettyBoy/wcs/'
 
 wcs_file = os.path.join(path, 'wcs.fits')
-
 f = fits.open(wcs_file)
+print(f)
 
+axy_file = os.path.join(path, 'axy.fits')
+f = fits.open(axy_file)
+print(f)
+
+corr_file = os.path.join(path, 'corr.fits')
+f = fits.open(corr_file)
+print(f)
+
+rdls_file = os.path.join(path, 'rdls.fits')
+f = fits.open(rdls_file)
 print(f)
